b5d77bb42cab12bd9284b4c734e90e86663a40d2
Jeff <jeffreydwyer95@hotmail.com>
2017-09-17 15:34:22 -0700

    Added problems for List

--- Lists/Problems.hs
+++ Lists/Problems.hs
@@ -1,2 +1,61 @@
 module Problems where
-
+
+{- Problem 1: Find the last element of a list
+   eg. problem1 [1,2,3,4,5]
+       5
+-}
+problem1 :: [a] -> a
+problem1 xs = undefined
+
+{- Problem 2: Find the second to last element of a list
+   eg. problem2 [1,2,3,4,5]
+       4
+-}
+problem2 :: [a] -> a
+problem2 xs = undefined
+
+{- Problem 3: Find the nth element of a list
+   eg. problem3 [1,2,3,4,5] 0
+       1
+
+       problem3 [1,2,3,4,5] 4
+       5
+
+       problem3 [1,2,3,4,5] 2
+       3
+-}
+problem3 :: [a] -> Int -> a
+problem3 xs n = undefined
+
+{- Problem 4: Find the length of a list
+   eg. problem4 [1,2,3,4]
+       4
+
+       problem4 [1]
+       1
+
+       problem4 []
+       0
+-}
+problem4 :: [a] -> Int
+problem4 xs = undefined
+
+{- Problem 5: Reverse a list
+   eg. problem5 [1,2,3]
+       [3,2,1]
+
+       problem5 []
+       []
+-}
+problem5 :: [a] -> [a]
+problem5 xs = undefined
+
+{- Problem 6: Determine if list is a palindrome
+   eg. problem6 [1,2,3]
+       false
+
+       problem6 [1,2,3,3,2,1]
+       true
+-}
+problem6 :: Eq a => [a] -> Bool
+problem6 xs = undefined
--- Lists/Tests.hs
+++ Lists/Tests.hs
@@ -1,13 +1,42 @@
+{-# LANGUAGE ScopedTypeVariables #-}
 module Main where
 
 import Test.Hspec
+import Test.QuickCheck
+import Control.Arrow
 
 import Problems
 
 main :: IO ()
 main = hspec $ do
-  describe "addition" $ do
-    context "should always" $ do
-      it "work" $
-        (1 + 1) `shouldBe` (2 :: Int)
-
+  describe "Problem 1" $ do
+    it "should get last element" $
+    property $ \(x :: [Int]) ->
+        problem1 x == last x
+        
+  describe "Problem 2" $ do
+    it "should get the second to last element" $
+      property $ \(x :: [Int]) ->
+        problem2 x == (last . init $ x)
+
+  describe "Problem 3" $ do
+    it "should get the nth element" $
+      property $ \(x :: [Int]) (n :: Int) ->
+        problem3 x n == (x !! n)
+
+  describe "Problem 4" $ do
+    it "should calculate the length of the list" $
+      property $ \(x :: [Int]) ->
+        problem4 x == length x
+
+  describe "Problem 5" $ do
+    it "should reverse the list" $
+      property $ \(x :: [Int]) ->
+        problem5 x == reverse x
+
+  describe "Problem 6" $ do
+    it "should find valid palindromes" $
+      property $ \(x :: Int) (isPal :: Bool) ->
+        let list = [1..x]
+            pal  = if isPal then list ++ reverse list else list
+        in  problem6 pal == isPal 
